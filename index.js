df5a7922394ddbef4faaf297406986f132f600bc
kik4 <kik4.contact@gmail.com>
2019-03-18 02:28:39 +0900

    initial

---
+++ index.js
@@ -0,0 +1,34 @@
+const width = 1920;
+const height = 1080;
+const r = 200;
+const xstep = 16;
+const ystep = 9;
+const pi2 = 2 * Math.PI;
+
+const xband = width / xstep;
+const yband = height / ystep;
+const xcenter = width / 2;
+const ycenter = height / 2;
+
+const canvas = document.getElementsByTagName("canvas")[0];
+canvas.width = width;
+canvas.height = height;
+
+const ctx = canvas.getContext("2d");
+ctx.fillRect(0, 0, 1920, 1080);
+
+ctx.strokeStyle = "white";
+ctx.fillStyle = "white";
+for (let y = 0; y < ystep; y++) {
+  for (let x = 0; x < xstep; x++) {
+    ctx.beginPath();
+    const xpos = (x + 0.5) * xband;
+    const ypos = (y + 0.5) * yband;
+    const xnorm = ((xpos - xcenter) / xcenter) ** 2;
+    const ynorm = ((ypos - ycenter) / ycenter) ** 2;
+    const rpos = r - r * Math.sqrt((xnorm + ynorm) / 2);
+    ctx.arc(xpos, ypos, rpos, 0, pi2, true);
+    ctx.fill();
+    ctx.closePath();
+  }
+}
