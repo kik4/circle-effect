b726922f63470aadecccb77177b7dc631b910c10
kik4 <kik4.contact@gmail.com>
2019-03-18 02:55:31 +0900

    compose

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const yband = height / ystep;
 const xcenter = width / 2;
 const ycenter = height / 2;
 
+//
 const canvas = document.getElementsByTagName("canvas")[0];
 canvas.width = width;
 canvas.height = height;
@@ -28,7 +29,8 @@ canvas.height = height;
       const ypos = (y + 0.5) * yband;
       const xnorm = ((xpos - xcenter) / xcenter) ** 2;
       const ynorm = ((ypos - ycenter) / ycenter) ** 2;
-      const rpos = r - r * Math.sqrt((xnorm + ynorm) / 2);
+      const d = 1 - Math.sqrt((xnorm + ynorm) / 2);
+      const rpos = r * d;
       ctx.arc(xpos, ypos, rpos, 0, pi2, true);
       ctx.fill();
       ctx.closePath();
@@ -36,6 +38,7 @@ canvas.height = height;
   }
 }
 
+//
 const canvas2 = document.getElementsByTagName("canvas")[1];
 canvas2.width = width;
 canvas2.height = height;
@@ -50,8 +53,35 @@ canvas2.height = height;
       ctx.beginPath();
       const xpos = (x + 0.5) * xband;
       const ypos = (y + 0.5) * yband;
-      const d = Math.abs(-xpos / width + ypos / height);
-      const rpos = (1 - d) * 100;
+      const d = 1 - Math.abs(-xpos / width + ypos / height);
+      const rpos = d * 100;
+      ctx.arc(xpos, ypos, rpos, 0, pi2, true);
+      ctx.fill();
+      ctx.closePath();
+    }
+  }
+}
+
+//
+const canvas3 = document.getElementsByTagName("canvas")[2];
+canvas3.width = width;
+canvas3.height = height;
+{
+  const ctx = canvas3.getContext("2d");
+  ctx.fillRect(0, 0, 1920, 1080);
+
+  ctx.strokeStyle = "white";
+  ctx.fillStyle = "white";
+  for (let y = 0; y < ystep; y++) {
+    for (let x = 0; x < xstep; x++) {
+      ctx.beginPath();
+      const xpos = (x + 0.5) * xband;
+      const ypos = (y + 0.5) * yband;
+      const xnorm = ((xpos - xcenter) / xcenter) ** 2;
+      const ynorm = ((ypos - ycenter) / ycenter) ** 2;
+      const d1 = 1 - Math.sqrt((xnorm + ynorm) / 2);
+      const d2 = 1 - Math.abs(-xpos / width + ypos / height);
+      const rpos = (d1 + d2) * 80;
       ctx.arc(xpos, ypos, rpos, 0, pi2, true);
       ctx.fill();
       ctx.closePath();
